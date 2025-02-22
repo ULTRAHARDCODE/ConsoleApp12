c3d382273993becd9890f2678d5515718667aa54
79191 <79191@MAXIM>
2025-02-22 19:45:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.Speech.Synthesis;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PlatformID version = Environment.OSVersion.Platform;
+            string str = Console.ReadLine();
+            AddVoice voice ;
+
+
+
+
+            if (version == PlatformID.Win32NT)
+            {
+                voice = speechOnWindows;
+            }
+            else 
+            {
+                voice = speechOnMacOS;
+            }
+            voice("privet");
+
+
+
+
+        }
+
+        delegate void AddVoice(string str);
+
+
+
+        static void speechOnWindows(string str)
+        {
+            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+
+            synthesizer.Volume = 100;
+            synthesizer.Rate = 0;
+
+            synthesizer.Speak(str);
+        }
+
+        static void speechOnMacOS(string msg)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "sox",
+                Arguments = msg,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = false
+            };
+
+
+
+            var process = Process.Start(startInfo);
+        }
+
+
+
+    }
+}
+
+
